2acda319e5edf3c300bcee97552a6b4bbaf743c3
Nadir <akh_nodir@mail.ru>
2024-04-14 16:39:01 +0500

    Create new array and fulfill with words <= 3 letters

--- Program.cs
+++ Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+
+        Console.WriteLine("Введите элементы массива через пробел:");
+        string input = Console.ReadLine()!;
+
+        string[] inputArray = input.Split(' ');
+
+        int newSize = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                newSize++;
+            }
+        }
+
+        string[] newArray = new string[newSize];
+
+
+        int index = 0;
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (inputArray[i].Length <= 3)
+            {
+                newArray[index] = inputArray[i];
+                index++;
+            }
+        }
+      
+        Console.ReadKey();
+    }
+}
